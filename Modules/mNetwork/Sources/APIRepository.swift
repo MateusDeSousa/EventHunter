aff1a07c76661a8707703d3f9575a9df39d1a342
Mateus Sousa <mateussousa@DBC-000840.local>
2020-12-14 21:47:12 -0300

    Adicionado metodo no API Repository para buscar evento especifico

--- Modules/mNetwork/Sources/APIRepository.swift
+++ Modules/mNetwork/Sources/APIRepository.swift
@@ -9,12 +9,30 @@ import Foundation
 
 class APIRepository {
     
-    func getEvents(completion: @escaping (Result<[EventModel], Error>) -> Void) {
-        let endPoint: String = "http://5f5a8f24d44d640016169133.mockapi.io/api/events"
-        if let url = URL(string: endPoint) {
+    let baseURL = "http://5f5a8f24d44d640016169133.mockapi.io/api"
+    
+    func getAllEvents(completion: @escaping (Result<[EventModel], Error>) -> Void) {
+        let endPoint = "/events"
+        if let url = URL(string: baseURL + endPoint) {
             do {
                 let data = try Data(contentsOf: url)
                 let listEvents = try JSONDecoder().decode([EventModel].self, from: data)
+                completion(.success(listEvents))
+            } catch {
+                completion(.failure(error))
+            }
+        }else {
+            let error = NSError()
+            completion(.failure(error))
+        }
+    }
+    
+    func getEvent(with id: Int, completion: (Result<EventModel, Error>) -> Void) {
+        let endPoint = "/events/\(id)"
+        if let url = URL(string: baseURL + endPoint) {
+            do {
+                let data = try Data(contentsOf: url)
+                let listEvents = try JSONDecoder().decode(EventModel.self, from: data)
                 completion(.success(listEvents))
             } catch {
                 completion(.failure(error))
--- Modules/mNetwork/mNetworkTests/APIRepositoryTests.swift
+++ Modules/mNetwork/mNetworkTests/APIRepositoryTests.swift
@@ -1,5 +1,5 @@
 //
-//  NetworkManagerTests.swift
+//  APIRepositoryTests.swift
 //  mNetworkTests
 //
 //  Created by Mateus Sousa on 14/12/20.
@@ -8,16 +8,33 @@
 import XCTest
 @testable import mNetwork
 
-class NetworkManagerTests: XCTestCase {
+class APIRepositoryTests: XCTestCase {
     
-    func testFetchEvents() {
+    func testGetAllEvents() {
         let expactation = XCTestExpectation(description: "get list of events")
         
         let api = APIRepository()
-        api.getEvents { (result) in
+        api.getAllEvents { (result) in
             switch result {
             case .success(let events):
                 XCTAssert(events.count > 0)
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+            expactation.fulfill()
+        }
+        
+        wait(for: [expactation], timeout: 4.0)
+    }
+    
+    func testFetchEvents() {
+        let expactation = XCTestExpectation(description: "get specific event")
+        
+        let api = APIRepository()
+        api.getEvent(with: 1) { (result) in
+            switch result {
+            case .success(let event):
+                XCTAssert(event.date == 1534784400)
             case .failure(let error):
                 XCTFail(error.localizedDescription)
             }
